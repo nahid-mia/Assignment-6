f5324f12449f2cc4c4b65ad6877629580c24e543
unknown <moh.nahidmia@gmail.com>
2026-03-30 02:51:35 +0600

    Fixing git repo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import GetStarted from './Components/GetStarted/GetStarted.jsx'
 import TransparentPricing from './Components/TransparentPricing/TransparentPricing.jsx'
 
 const fetchData = async() => {
-  const res = await fetch("/data.json");
+  const res = await fetch("data.json");
   return res.json();
 }
 
